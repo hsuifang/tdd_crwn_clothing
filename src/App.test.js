e7e9b4607722f1c2d009fa9c3287e5ef03b54d86
hsuifang <whalekiller03@gmail.com>
2023-11-22 23:44:57 +0800

    feat: init practice project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  describe("UI", () => {
+    test("Has five category", () => {
+      render(<App />);
+      const category = screen.getAllByTestId("category");
+      expect(category).toHaveLength(5);
+    });
+
+    test("each content in category", () => {
+      render(<App />);
+      const category = screen.getAllByTestId("category");
+      const lists = ["Hats", "Jackets", "Sneakers", "Womens", "Mens"];
+      category.forEach((item, idx) => {
+        expect(item).toHaveTextContent(lists[idx]);
+      });
+    });
+  });
 });
